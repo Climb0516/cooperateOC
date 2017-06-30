5710eee24d359de222c8dc79e4df578f8a3672d7
wangpandeng <wangpandeng@waqu.com>
2017-06-30 17:13:34 +0800

    First Push

---
+++ cooperateOC/cooperateOC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  cooperateOC
+//
+//  Created by 王攀登 on 2017/6/30.
+//  Copyright © 2017年 王攀登. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ cooperateOC/cooperateOC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cooperateOC
+//
+//  Created by 王攀登 on 2017/6/30.
+//  Copyright © 2017年 王攀登. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
